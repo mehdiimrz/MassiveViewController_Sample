7e70381ccbdcca1123f09775ab14294f2bdd8582
Mehdi Mirzaie <Mehdiimrz@gmail.com>
2020-08-25 19:15:29 +0430

    TableView added to HomeDogVC

--- MassiveViewContoller_Sample/Activities/HomeDog/HomeDogViewController.swift
+++ MassiveViewContoller_Sample/Activities/HomeDog/HomeDogViewController.swift
@@ -10,6 +10,47 @@ import UIKit
 
 class HomeDogViewController: UIViewController {
     
+    // MARK: Properties
     
+    private var dogsData = [HomeDog](){
+        didSet{
+            dogsTable.reloadData()
+        }
+    }
+    
+    private lazy var dogsTable : UITableView = {
+        
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .lightGray
+        tableView.rowHeight = 110
+        tableView.register(HomeDogTableViewCell.self, forCellReuseIdentifier: "DogCell")
+        return tableView
+    }()
+    
+    
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        do {
+            try dogsData = HomeDog.createHome()
+        } catch  {
+            print("Error : \(error.localizedDescription)")
+        }
+    }
 }
 
+// MARK: Helper
+extension HomeDogViewController{
+    
+    private func setup(){
+        
+        // TableView
+        view.addSubview(dogsTable)
+        dogsTable.constrainEdges(to: view)
+        
+    }
+}

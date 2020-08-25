72678431b8034257c9e0e65c68756312df090068
Mehdi Mirzaie <Mehdiimrz@gmail.com>
2020-08-26 01:04:25 +0430

    Array extension added

---
+++ MassiveViewContoller_Sample/Helper/Extensions/Array+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Array+Extension.swift
+//  MassiveViewContoller_Sample
+//
+//  Created by Mehdi-Mac on 8/26/20.
+//  Copyright © 2020 Mehdiimrz. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    func pick(_ n: Int) -> [Element] {
+        guard count >= n else {
+            fatalError("The count has to be at least \(n)")
+        }
+        guard n >= 0 else {
+            fatalError("The number of elements to be picked must be positive")
+        }
+
+        let shuffledIndices = indices.shuffled().prefix(upTo: n)
+        return shuffledIndices.map {self[$0]}
+    }
+}
